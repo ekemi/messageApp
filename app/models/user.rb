578f7c22cc8de285affcaec1f4f3a5c1e5afa1c7
Yapnounkie <edmundopierre@yahoo.com>
2020-12-14 01:53:38 -0800

    add message partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
-
-    validates :username, presence: true , uniqueness: true, length: {minimum:3 , maximum: 15}
+     has_many :messages
+    validates :username, presence: true , uniqueness:{case_sensitive: false}, length: {minimum:3 , maximum: 15}
 end
